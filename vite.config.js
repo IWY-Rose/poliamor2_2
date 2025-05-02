860de3c6f4b81c38d50344684528c8623460d601
Ignacio Willson Yáñez <ignacio.willson@gmail.com>
2025-05-02 02:36:19 -0400

    Deploy_0.1

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     base: './',
@@ -6,6 +7,10 @@ export default defineConfig({
     build: {
         assetsInlineLimit: 4096, // Optimize asset inlining
         rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                game: resolve(__dirname, 'game.html')
+            },
             output: {
                 manualChunks: {
                     three: ['three']
